6674fce6230e93a5ca5a51a80974ac6b20381ab7
global <global@del1-lhp-n01920.synapse.com>
2020-09-07 16:08:37 +0530

    My first commit

---
+++ src/test/java/FreshMavenProject/LRSeptember/GetRequest.java
@@ -0,0 +1,18 @@
+package FreshMavenProject.LRSeptember;
+
+import static com.jayway.restassured.RestAssured.*;
+
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+
+public class GetRequest {
+
+	public static void main(String[] args) 
+	{
+		Response res  = given().contentType(ContentType.JSON).when().get("http://restapi.demoqa.com/utilities/weather/city");
+		
+		System.out.println("Status code is::" +res.statusCode());
+
+	}
+
+}
